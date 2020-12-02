bc423b9228397ab3d43c9c318363208cfeb9af81
VladyslavKvaskov <vladkvaskov@yahoo.com>
2020-12-02 17:41:36 +0200

    update

---
+++ linkify-element.js
@@ -0,0 +1,161 @@
+class LinkifyElement extends HTMLElement {
+  #urlRegex = /(https?:\/\/[^\s]+)/g;
+  #linkify = (text) => {
+    return text.replace(/([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/.\?\=\&\#]?[\w-]+)*\/?/gmi, (a) => {
+      a = a.trim();
+      return `<a href="${!a.startsWith('http://') && !a.startsWith('https://') ? `http://${a}` : a}" target="_blank" class="url-from-txt">${a}</a>`;
+    });
+  }
+
+  #validJSON = false;
+  #linksPreviewer = document.createElement('expandable-element');
+  #prevAnchors = [];
+  #previewsLimit = -1;
+  #previewsCounter;
+  #tmpHTML = '';
+  #i;
+  #fetchURL = () => {
+    this.urlsToFetch = super.querySelectorAll('a');
+    this.anchorHrefs = [];
+    this.anchors = [];
+    for (this.d of this.urlsToFetch) {
+      this.anchors.push(this.d);
+      this.anchorHrefs.push(this.d.href);
+    }
+
+
+    if (this.anchors.length > 0 && this.#prevAnchors.join() != this.anchorHrefs.join()) {
+      this.fData = new FormData();
+      this.fData.append('links', JSON.stringify(this.anchorHrefs));
+
+      console.log(location.hostname);
+      fetch(location.hostname === 'linkify-element' ? '../links-info.php' : 'https://linkify-element.vercel.app/links-info.php', {
+          method: 'POST',
+          body: this.fData
+        }).then(r => r.text())
+        .then(data => {
+          try {
+            data = JSON.parse(data);
+            this.#validJSON = true;
+          } catch (err) {
+            this.#validJSON = false;
+          }
+
+          if (this.#validJSON) {
+            this.#previewsCounter = 0;
+            this.previewed = false;
+            this.#tmpHTML = '';
+            for (this.#i = 0; this.#i < data.length; this.#i++) {
+              this.d = data[this.#i];
+              this.anchor = this.anchors[this.#i];
+              if (this.anchor && this.anchor.href == this.d.original_url) {
+                if (this.d.not_found) {
+                  this.anchor.classList.add('not-found');
+                } else {
+                  if ((this.d.favicon || this.d.title || this.d.description) && (this.#previewsCounter < this.#previewsLimit || this.#previewsLimit == -1)) {
+                    this.#tmpHTML += `
+                      <div class="link-info">
+                        ${this.d.favicon || data.host ? `<div class="link-t">${this.d.favicon ? `<span class="fav"><span class="link-favicon" style="background-image:url('${this.d.favicon}')"></span></span>`:''} ${this.d.host ? `<span class="link-host">${this.d.host}</span>` : ''}</div>` : ''}
+                        ${this.d.title ? `<div class="link-title"><a href="${this.anchor.href}" target="_blank">${this.d.title}</a></div>` : ''}
+                        ${this.d.description ? `<div class="link-description">${this.d.description}</div>` : ''}
+                      </div>
+                    `;
+                  }
+
+                  this.#previewsCounter++;
+                }
+              }
+
+              if (this.#previewsCounter === this.#previewsLimit) {
+                break;
+              }
+            }
+
+            if (this.#linksPreviewer.innerHTML != this.#tmpHTML) {
+              this.#linksPreviewer.innerHTML = this.#tmpHTML;
+            }
+          }
+        });
+    } else if (this.anchors.length === 0) {
+      this.#linksPreviewer.innerHTML = '';
+    }
+
+    this.#prevAnchors = this.anchorHrefs;
+  }
+
+  constructor() {
+    super();
+    this.#linksPreviewer.classList.add('link-previewer');
+    this.after(this.#linksPreviewer);
+    super.innerHTML = this.#linkify(this.innerText.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+    this.#fetchURL();
+  }
+
+  get innerHTML() {
+    return super.innerHTML;
+  }
+  set innerHTML(html) {
+    super.innerHTML = this.#linkify(html.replace(/<[^>]*>?/gm, ''));
+    this.#fetchURL();
+  }
+}
+
+customElements.define('linkify-element', LinkifyElement);
+
+
+
+class ExpandableElement extends HTMLElement {
+  #viewMoreButton = document.createElement('button');
+  constructor() {
+    super();
+    this.#viewMoreButton.classList.add('view-more');
+    this.#viewMoreButton.type = 'button';
+
+    this.#viewMoreButton.onclick = () => {
+      if (this.previews.length > 0) {
+        this.#viewMoreButton.classList.toggle('expanded');
+        if (this.#viewMoreButton.classList.contains('expanded')) {
+          this.height = 0;
+          for (this.d of this.querySelectorAll('div.link-info')) {
+            this.height += this.d.offsetHeight + 20;
+          }
+
+          this.#viewMoreButton.innerHTML = 'view less';
+          this.style.height = this.height + 'px';
+        } else {
+          this.style.height = this.previews[0].offsetHeight + 20 + 'px';
+          this.#viewMoreButton.innerHTML = 'view more';
+        }
+      }
+    }
+  }
+
+  get innerHTML() {
+    return super.innerHTML;
+  }
+
+  set innerHTML(html) {
+    super.innerHTML = html;
+    if (this.innerHTML.trim() != '') {
+      this.previews = this.querySelectorAll('div.link-info');
+      if (this.previews.length > 0) {
+        this.style.height = this.previews[0].offsetHeight + 20 + 'px';
+        if (this.previews.length > 1) {
+          this.#viewMoreButton.innerHTML = 'view more';
+          this.#viewMoreButton.classList.remove('expanded');
+          this.after(this.#viewMoreButton);
+        } else {
+          this.#viewMoreButton.remove();
+        }
+      }
+    } else {
+      this.#viewMoreButton.remove();
+    }
+  }
+
+  // attributeChangedCallback(attrName, oldVal, newVal) {
+  //   console.log(attrName);
+  // }
+}
+
+customElements.define('expandable-element', ExpandableElement);
---
+++ links-info.php
@@ -0,0 +1,86 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  $json = [];
+
+  header("Access-Control-Allow-Origin: *");
+  header('Content-Type: application/json');
+
+  if(!empty($_REQUEST['links']) && isset($_REQUEST['links'])){
+    $urls = json_decode($_REQUEST['links']);
+    $arrContextOptions=array(
+      'ssl'=>array(
+          'verify_peer'=>false,
+          'verify_peer_name'=>false,
+
+      ),
+      'http'=>array(
+        'header' => "Accept-language: en\r\n" . "User-Agent: {$_SERVER["HTTP_USER_AGENT"]}\r\n"
+      ),
+    );
+
+    for($i = 0; $i < count($urls); $i++){
+      $obj['favicon'] = null;
+      $obj['title'] = null;
+      $obj['description'] = null;
+      $obj['host'] = null;
+      $obj['original_url'] = $urls[$i];
+      $obj['not_found'] = false;
+
+      [$url,] = explode('?', $urls[$i]);
+      $url = $url[strlen($url) - 1] !== '/' ? $url . '/' : $url;
+      $url_info = parse_url($url);
+      $obj['host'] = $url_info['host'];
+
+      $favicon = @file_get_contents($url . 'favicon.ico', false, stream_context_create($arrContextOptions));
+      if(!empty($favicon)){
+        $obj['favicon'] = $url . 'favicon.ico';
+      }
+
+      $html = @file_get_contents($url, false, stream_context_create($arrContextOptions));
+
+      if(!empty($html)){
+        if(empty($favicon)){
+          $gicon = @file_get_contents('https://www.google.com/s2/favicons?domain=' . $url, false, stream_context_create($arrContextOptions));
+          $obj['favicon'] = 'https://www.google.com/s2/favicons?domain=' . $url;
+        }
+
+        $title = parseTitle($html);
+        if(!empty($title)){
+          $obj['title'] = $title;
+        }
+        $description = parseDescription($html);
+        if(!empty($description)){
+          $obj['description'] = $description;
+        }
+      }
+      else{
+        $obj['not_found'] = true;
+      }
+
+      $json[] = $obj;
+    }
+  }
+
+  function parseTitle($html) {
+    $title = preg_match('/<title[^>]*>(.*?)<\/title>/ims', $html, $match) ? $match[1] : null;
+    return $title;
+  }
+
+  function parseDescription($html) {
+    $matches = array();
+    preg_match('/<meta.*?name=("|\').*description("|\').*?content=("|\')([^"]+)("|\')/i', $html, $matches);
+    if (count($matches) > 4) {
+      return trim($matches[4]);
+    }
+    preg_match('/<meta.*?content=("|\')([^"]+)("|\').*?name=("|\').*description("|\')/i', $html, $matches);
+    if (count($matches) > 2) {
+      return trim($matches[2]);
+    }
+
+    return null;
+  }
+
+  print_r(json_encode($json));
+?>
